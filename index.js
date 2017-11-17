540b4c274ead1d3d51ebe7f1bf91264e199fa09d
York Wang <yw544@nyu.edu>
2017-11-17 22:39:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-let companyName = 'Scuber';
+it('is set as Scuber', function () {
+  expect(companyName).to.equal('Scuber');
+});
 let mostProfitableNeighborhood = "Chelsea";
 let companyCeo = "Susan Smith";
